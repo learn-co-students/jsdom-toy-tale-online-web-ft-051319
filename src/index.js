cd41090c40bba284e40c3caff061e59c2aa7c85e
travis_exline <travisexline@Ellyn-PC.localdomain>
2019-08-28 11:30:36 -0500

    working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
 let addToy = false
 
-// YOUR CODE HERE
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(addToyToCard)
+}
+
+function addToyToCard(json) {
+  json.forEach(toy => {
+    toyCollection.innerHTML += `<div class="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+    </div>`
+  })
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
