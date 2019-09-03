19d9d62721e1de84412a788f5e3cb1968bb35030
Kane Koo <kanekoo@gmail.com>
2019-09-03 00:55:29 -0400

    finished first form read, onto second

--- src/index.js
+++ src/index.js
@@ -14,5 +14,52 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+document.addEventListener("DOMContentLoaded", ()=> {
+  var toyURL="http://localhost:3000/toys";
+  const toyCollection = document.getElementById("toy-collection");
+  fetch(toyURL)
+  .then(resp => resp.json())
+  .then(json => {
+  //  debugger;
+    for (const jsElement of json) {
+      toyCollection.innerHTML += `<div class="card">\
+      <h2>${jsElement["name"]}</h2> \
+      <img src=${jsElement["image"]} class="toy-avatar" /> \
+      <p>${jsElement["likes"]} Likes</p> \
+      <button class="like-btn">Like <3</button> \
+      </div>`
+
+      }
+    });
+    //  toyCollection.innerHTML += ``
+  //    toyCollection.innerHTML += '</div>'
+      //debugger;
+
+
+  })
+
+toyForm.addEventListener("submit", function newToy(e)  {
+//  debugger;
+  e.preventDefault();
+
+  let inputText = toyForm.getElementsByClassName("input-text");
+  let configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+      },
+    body: JSON.stringify({
+      "name": `${inputText.name.value}`,
+      "image": `${inputText.image.value}`,
+      "likes": 0
+      })
+    };
+
+  return fetch("http://localhost:3000/toys", configObject)
+    .then (resp => resp.json())
+    .then (json => console.log(json));
+  });
+
 
 // OR HERE!
